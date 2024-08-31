ca003a45332a644cdb3b979c9d48bcadd07e7877
Maayan56 <maayansha12@gmail.com>
2024-08-31 18:34:28 +0300

    add export to pdf

--- src/pages/AllSchedulesPage.jsx
+++ src/pages/AllSchedulesPage.jsx
@@ -1,5 +1,7 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import CoursesScheduler from "../components/CoursesScheduler.jsx";
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 const AllSchedulesPage = () => {
     const [selectedSemester, setSelectedSemester] = useState('');
 
@@ -26,20 +28,46 @@ const AllSchedulesPage = () => {
 
     const coursesProps = getCoursesProps();
 
+    const printRef = useRef();
+    const generatePDF = () => {
+        const input = printRef.current;
+
+        html2canvas(input).then((canvas) => {
+            const imgData = canvas.toDataURL('image/png');
+            const pdf = new jsPDF('p', 'mm', 'a4',true);
+            const pdfWidth = pdf.internal.pageSize.getWidth();
+            const pdfHeight = pdf.internal.pageSize.getHeight();
+            const imgWidth = canvas.width;
+            const imgHeight = canvas.height;
+            const ratio = Math.min(pdfWidth / imgWidth, pdfHeight / imgHeight);
+            const imgX= (pdfWidth - imgWidth * ratio) / 2;
+            const imgY= 30;
+            pdf.addImage(imgData, 'PNG', imgX, imgY, imgWidth*ratio, imgHeight*ratio);
+
+            pdf.save('your_scheduler.pdf');
+        });
+    };
+
     return (
         <div>
-            <select className='border rounded w-1/8 py-2 px-3 mb-2' value={selectedSemester} onChange={handleChange}>
-                <option value="">Select a semester</option>
-                <option value="A">Semester A</option>
-                <option value="B">Semester B</option>
-            </select>
+            <div ref={printRef} id="content-to-print">
+                <select className='border rounded w-1/8 py-2 px-3 mb-2' value={selectedSemester} onChange={handleChange}>
+                    <option value="">Select a semester</option>
+                    <option value="A">Semester A</option>
+                    <option value="B">Semester B</option>
+                </select>
 
-            {coursesProps && (
-                <CoursesScheduler
-                    reqCoursesArrayName={coursesProps.reqCoursesArrayName}
-                    choiceCoursesArrayName={coursesProps.choiceCoursesArrayName}
-                />
-            )}
+                {coursesProps && (
+                    <CoursesScheduler
+                        reqCoursesArrayName={coursesProps.reqCoursesArrayName}
+                        choiceCoursesArrayName={coursesProps.choiceCoursesArrayName}
+                    />
+                )}
+            </div>
+            {coursesProps && (<button className='block mx-auto bg-indigo-500 hover:bg-indigo-600 text-white mt-2 font-bold py-2 px-4 rounded-full w-1/6 focus:outline-none focus:shadow-outline'
+                    onClick={generatePDF}>
+                Download as PDF
+            </button>)}
         </div>
     );
 };
